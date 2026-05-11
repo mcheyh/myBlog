1c37203ee6541a75094490dfd9477f31027e83df
Yuhang He <3099642736@qq.com>
2026-05-11 21:32:35 +0800

    Create education.js

---
+++ education.js
@@ -0,0 +1,25 @@
+const educations = [
+  {
+    school: "To be or not to be",
+    link: "https://www.tsinghua.edu.cn/",
+    degree: "ToDo",
+    status: "ToDo",
+    major: "ToDo"
+  },
+
+  {
+    school: "西北工业大学集成电路学院",
+    link: "https://weidianzi.nwpu.edu.cn/",
+    degree: "本科",
+    status: "工学学士在读",
+    major: "集成电路设计与集成系统"
+  },
+
+  {
+    school: "江苏省昆山中学",
+    link: "https://kz.ksecloud.cn",
+    degree: "高中",
+    status: "高中学历",
+    major: ""
+  }
+];
