53b0a4502b5de6109389034d4e68a6fc6f8e0473
Yuhang He <3099642736@qq.com>
2026-05-11 21:38:44 +0800

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,18 @@
+const contacts = [
+  {
+    type: "电子邮箱",
+    value: "mcheyh [at] qq [dot] com"
+  },
+
+  {
+    type: "GitHub",
+    value: "mcheyh",
+    link: "https://github.com/mcheyh"
+  },
+
+  {
+    type: "博客",
+    value: "个人主页",
+    link: "https://mcheyh.github.io/"
+  }
+];
