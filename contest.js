c52dcc9b8f57c0686ee4c62bc9c8da9cca4e7d2c
Yuhang He <3099642736@qq.com>
2026-05-11 21:27:27 +0800

    Create contest.js

---
+++ contest.js
@@ -0,0 +1,12 @@
+const contests = [
+  {
+    time: "2022年9月 — 2025年6月",
+    organization: "江苏省昆山中学 信息学竞赛",
+    link: "https://oier.baoshuo.dev/oier/164863",
+    awards: [
+      ["NOIP", "省二 2 次"],
+      ["CSP-S", "二等 2 次"],
+      ["CSP-J", "一等 1 次"]
+    ]
+  }
+];
