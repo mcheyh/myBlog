2c10d3af514fe86b33bf6ff4475b02478b8ad581
Yuhang He <3099642736@qq.com>
2026-05-16 13:26:22 +0800

    Update contest.js

--- contest.js
+++ contest.js
@@ -2,12 +2,9 @@ const contests = [
   {
     time: "2023年9月 — 2024年12月",
     organization: "江苏省昆山中学 信息学竞赛",
-    link: "https://oier.baoshuo.dev/oier/164863",
-    linkName: "OIerDb",
-
     awards: [
-      ["NOIP", "二等奖"],
-      ["CSP-S", "二等"]
+      ["NOIP", "二等奖","https://oier.baoshuo.dev/oier/164863","OIerDb"],
+      ["CSP-S", "二等","https://oier.baoshuo.dev/oier/164863","OIerDb"]
     ]
   },
 
@@ -22,11 +19,8 @@ const contests = [
 
   {
     time: "2026年1月",
-    organization: "西北工业大学",
-    link: "https://www.comap-math.org/mcm/2026Certs/2626001.pdf",
-    linkName: "证书",
     awards: [
-      ["国际大学生数学建模竞赛", "Honorable Mention"]
+      ["国际大学生数学建模竞赛", "Honorable Mention","https://www.comap-math.org/mcm/2026Certs/2626001.pdf","证书"]
     ]
   }
 ];
